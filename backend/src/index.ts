d704cf2562b8c50571fc9c579ac883c87ca3bda4
Benoît Hubert <benoithubert@gmail.com>
2022-01-19 08:53:06 +0100

    Replace hardcoded database URL with a variable-based value

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,13 @@ import { connect } from 'mongoose';
 import cors from 'cors';
 import { router as apiRouter } from './routes';
 
-connect("mongodb://127.0.0.1:27017/linksdb")
+// Utilisation d'une variable d'environnement avec une valeur
+// par défaut (sera utile plus tard pour faire communiquer le back
+// avec la db Mongo)
+const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
+const mongoUrl = `mongodb://${mongoHost}:27017/linksdb`;
+
+connect(mongoUrl)
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log(err));
 
@@ -18,6 +24,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', apiRouter);
 
+// Route juste pour tester le back sans la database
 app.get('/message', (req, res) => {
   res.send({ message: 'Hello from Express' });
 });
